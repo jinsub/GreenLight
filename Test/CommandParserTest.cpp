80b9e88b21d86da029b2b27623284a35279520a6
양진섭 <elliottful@gmail.com>
2022-02-15 15:27:04 +0900

    remove test file

--- Test/CommandParserTest.cpp
+++ Test/CommandParserTest.cpp
@@ -6,21 +6,20 @@ class CommandPerserTest : public testing::Test
 protected:
 	void SetUp(void) override
 	{
-	}
-	void TearDown(void) override
-	{
-	}
-
-	void _MakeSampleInput(const char* fileName, const int inputCount)
-	{
-		auto file = fopen(fileName, "w");
-		for (int sampleNum = 0; sampleNum < inputCount; sampleNum++)
+		auto file = fopen(fileName_, "w");
+		for (int sampleNum = 0; sampleNum < inputCount_; sampleNum++)
 		{
 			fprintf(file, "ADD, , , ,%d,JINSEOB YANG,CL2,010-2644-6239,19920616,PRO\n", 17106626 + sampleNum);
 		}
 		fclose(file);
 	}
+	void TearDown(void) override
+	{
+		std::remove(fileName_);
+	}
 
+	static constexpr char* fileName_ = "test.txt";
+	static constexpr int inputCount_ = 10;
 	CommandParser parser_;
 };
 
@@ -30,20 +29,12 @@ TEST_F(CommandPerserTest, Construct)
 
 TEST_F(CommandPerserTest, ParseFile)
 {
-	constexpr int InputCount = 10;
-	auto fileName = "input.text";
-	_MakeSampleInput(fileName, InputCount);
-
-	auto result = parser_.ParseFile(fileName);
-	EXPECT_EQ(result.size(), InputCount);
+	auto result = parser_.ParseFile(fileName_);
+	EXPECT_EQ(result.size(), inputCount_);
 }
 
 TEST_F(CommandPerserTest, ParseLine)
 {
-	constexpr int InputCount = 10;
-	auto fileName = "input.text";
-	_MakeSampleInput(fileName, InputCount);
-
-	auto result = parser_.ParseLine(parser_.ParseFile(fileName));
-	EXPECT_EQ(result.size(), InputCount);
+	auto result = parser_.ParseLine(parser_.ParseFile(fileName_));
+	EXPECT_EQ(result.size(), inputCount_);
 }
