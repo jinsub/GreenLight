3b7417fc15cd3bf8e9d9fae919012abaac4e94b7
윤성호 <hiptionjjang@gmail.com>
2022-02-16 14:52:09 +0900

    [GREEN] pass all initial tests reated with read

--- GreenLight/MemoryDatabase.cpp
+++ GreenLight/MemoryDatabase.cpp
@@ -1,5 +1,6 @@
 #include "MemoryDatabase.h"
 #include <vector>
+#include <string>
 using namespace std;
 
 vector<EmployeeInfo> MemoryDatabase::CreateDB(EmployeeInfo info) {
@@ -27,32 +28,42 @@ vector<EmployeeInfo> MemoryDatabase::ReadDB(TargetParam filter) {
 	vector<EmployeeInfo> result;
 	vector<unsigned int> nums;
 
-
 	switch (filter.column) {
 	case Column::EmployeeNum:
+		result.push_back(map_.mainDB_[stoul(filter.value, nullptr, 0)]);
 		break;
 	case Column::Name:
+		nums = getEmployeeNums(map_.fullName_Map_, filter.value);
 		break;
 	case Column::CareerLevel:
+		nums = getEmployeeNums(map_.career_Map_, filter.value);
 		break;
 	case Column::PhoneNumber:
+		nums = getEmployeeNums(map_.fullPhone_Map_, filter.value);
 		break;
 	case Column::Birthday:
+		nums = getEmployeeNums(map_.birth_Map_, filter.value);
 		break;
 	case Column::Certi:
+		nums = getEmployeeNums(map_.cert_Map_, filter.value);
 		break;
 	case Column::FirstName:
 		nums =  getEmployeeNums(map_.firstName_Map_, filter.value);		
 		break;
 	case Column::LastName:
+		nums = getEmployeeNums(map_.lastName_Map_, filter.value);
 		break;
 	case Column::MiddlePhoneNum:
+		nums = getEmployeeNums(map_.midPhone_Map_, filter.value);
 		break;
 	case Column::LastPhoneNum:
+		nums = getEmployeeNums(map_.lastPhone_Map_, filter.value);
 		break;
 	case Column::BirthdayYear:
+		nums = getEmployeeNums(map_.birthYear_Map_, filter.value);
 		break;
 	case Column::BirthdayMonth:
+		nums = getEmployeeNums(map_.birthMonth_Map_, filter.value);
 		break;
 	case Column::BirthdayDay:
 		nums = getEmployeeNums(map_.birthDay_Map_, filter.value);
